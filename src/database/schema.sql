63adc07cca625baa411a2336b445b9ed11baf8e8
esevangelista <esevangelista>
2018-03-10 14:33:54 +0800

    Modify schema for user

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -12,16 +12,16 @@ CREATE TABLE user(
   employeeID VARCHAR (30) NOT NULL PRIMARY KEY,
   password VARCHAR (36) NOT NULL,
   firstName VARCHAR (50) NOT NULL,
-  middleName VARCHAR (50) NOT NULL,
+  middleName VARCHAR (50) ,
   lastName VARCHAR (50) NOT NULL,
-  committee VARCHAR (30) NOT NULL,
-  isHead VARCHAR (5) NOT NULL,      -- YES / NO
+  committee VARCHAR (30),
+  isHead TINYINT(1) NOT NULL,      -- TRUE / FALSE
   officeNumber VARCHAR (30) NOT NULL, 
   contractType VARCHAR (40) NOT NULL, -- FULL-TIME / PART-TIME
   emailAddress VARCHAR (40) NOT NULL,
   rank VARCHAR (30) NOT NULL,
   isArchived VARCHAR (10) NOT NULL, -- YES / NO / PENDING (?) 
-  acctType VARCHAR(10) -- ADMIN / USER
+  acctType VARCHAR(10) NOT NULL -- ADMIN / USER
 );
 -- Privileges
 GRANT SUPER ON *.* TO 'easyfsr'@'localhost';
