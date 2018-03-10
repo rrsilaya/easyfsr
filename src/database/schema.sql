b1e5757772fafb926e50fb81efd310c140ca0ace
esevangelista <esevangelista>
2018-03-10 13:30:40 +0800

    Include initial db schema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -8,5 +8,21 @@ CREATE DATABASE fsrmgtsys;
 
 USE fsrmgtsys;
 
+CREATE TABLE user(
+  employeeID VARCHAR (30) NOT NULL PRIMARY KEY,
+  password VARCHAR (36) NOT NULL,
+  firstName VARCHAR (50) NOT NULL,
+  middleName VARCHAR (50) NOT NULL,
+  lastName VARCHAR (50) NOT NULL,
+  committee VARCHAR (30) NOT NULL,
+  isHead VARCHAR (5) NOT NULL,      -- YES / NO
+  officeNumber VARCHAR (30) NOT NULL, 
+  contractType VARCHAR (40) NOT NULL, -- FULL-TIME / PART-TIME
+  emailAddress VARCHAR (40) NOT NULL,
+  rank VARCHAR (30) NOT NULL,
+  isArchived VARCHAR (10) NOT NULL, -- YES / NO / PENDING (?) 
+  acctType VARCHAR(10) -- ADMIN / USER
+);
 -- Privileges
+GRANT SUPER ON *.* TO 'cmsc128-a6l'@'localhost';
 GRANT ALL PRIVILEGES ON fsrmgtsys.* TO 'fsrmgtsys'@'localhost';
