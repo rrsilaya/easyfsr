05aa727ce3f267581bee53ab5ecb214fa9afd622
prastudillo <prastudillo@up.edu.ph>
2018-03-12 23:49:16 +0800

    Initial design for dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { Icon, Button, Card, Progress, Table } from 'antd';
+import styles from './styles';
+import actions from './actions';
+import dataSource from './datasource';
+import columns from './columns';
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
+        <Card
+          title="Announcements"
+          style={styles.announcement}
+          actions={actions.map(action => <Icon type={action} />)}
+        >
+          No announcements available
+        </Card>
+        <div>
+          <Card title="Faculty Progress">
+            <Progress
+              type="dashboard"
+              percent={30}
+              style={styles.progressBar}
+            />
+            <Table
+              columns={columns}
+              dataSource={dataSource}
+              style={styles.facultyTable}
+            />
+          </Card>
+        </div>
       </div>
     );
   }
---
+++ src/pages/dashboard/styles.js
@@ -0,0 +1,25 @@
+const style = {
+  announcement: {
+    marginBottom: '10px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  addButton: {
+    justifyContent: 'center',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  progressBar: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 600,
+  },
+  facultyTable: {
+    marginTop: 20,
+    size: 'large',
+  },
+};
+
+export default style;
