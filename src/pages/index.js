77189c5e4c96003f16ddbb32166e526b2caf5292
rrsilaya <rrsilaya@up.edu.ph>
2018-03-12 15:27:22 +0800

    Connect container to redux

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,11 @@ import { routerReducer as router } from 'react-router-redux';
 /**
  * Import your reducers here. (They are commonly inside duck.js.)
  */
+import app from '../app/duck';
 
 const reducers = combineReducers({
   router,
+  app,
 });
 
 export default reducers;
