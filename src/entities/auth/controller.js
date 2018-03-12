f6202f21a9ada09ac2887e2a9501ffcccb35e6d3
rrsilaya <rrsilaya@up.edu.ph>
2018-03-12 09:38:22 +0800

    Change query placeholders to object keys

--- src/entities/auth/controller.js
+++ src/entities/auth/controller.js
@@ -3,12 +3,14 @@ import bcrypt from 'bcrypt';
 
 export const login = ({ emailAddress, password }) => {
   return new Promise((resolve, reject) => {
-    const query = `SELECT * FROM user WHERE emailAddress = ?`;
-    const values = [emailAddress];
+    const query = `
+      SELECT * FROM user
+      WHERE emailAddress = :emailAddress
+    `;
 
-    db.query(query, values, (err, result) => {
+    db.query(query, { emailAddress }, (err, result) => {
       if (err) return reject(500);
-      else if (!result.length) return reject(404);
+      else if (!result.length) return reject(401);
 
       bcrypt.compare(password, result[0].password, (error, isMatch) => {
         if (error) return reject(500);
--- src/entities/user/controller.js
+++ src/entities/user/controller.js
@@ -1,94 +1,52 @@
 import db from './../../database/index';
 
-export const addUser = ({
-  employeeID,
-  password,
-  firstName,
-  middleName,
-  lastName,
-  committee,
-  isHead,
-  officeNumber,
-  contractType,
-  emailAddress,
-  rank,
-  isArchived,
-  acctType,
-}) => {
+export const addUser = user => {
   return new Promise((resolve, reject) => {
-    const query = `INSERT INTO user VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-    const values = [
-      employeeID,
-      password,
-      firstName,
-      middleName,
-      lastName,
-      committee,
-      isHead,
-      officeNumber,
-      contractType,
-      emailAddress,
-      rank,
-      isArchived,
-      acctType,
-    ];
+    const query = `
+      INSERT INTO user VALUES (
+        :employeeID, 
+        :password, 
+        :firstName, 
+        :middleName, 
+        :lastName, 
+        :committee, 
+        :isHead, 
+        :officeNumber, 
+        :contractType, 
+        :emailAddress, 
+        :rank, 
+        :isArchived, 
+        :acctType
+      )
+    `;
 
-    db.query(query, values, (err, results) => {
+    db.query(query, user, (err, results) => {
       if (err) return reject(500);
       return resolve(results.insertId);
     });
   });
 };
 
-export const updateUser = (
-  { employeeID },
-  {
-    password,
-    firstName,
-    middleName,
-    lastName,
-    committee,
-    isHead,
-    officeNumber,
-    contractType,
-    emailAddress,
-    rank,
-    isArchived,
-    acctType,
-  },
-) => {
+export const updateUser = ({ employeeID }, user) => {
   return new Promise((resolve, reject) => {
-    const query = `UPDATE user SET 
-      password = ?,
-      firstName = ?,
-      middleName = ?,
-      lastName = ?,
-      committee = ?,
-      isHead = ?,
-      officeNumber = ?,
-      contractType = ?,
-      emailAddress = ?,
-      rank = ?,
-      isArchived = ?,
-      acctType = ? 
-      WHERE employeeID = ?;`;
-    const values = [
-      password,
-      firstName,
-      middleName,
-      lastName,
-      committee,
-      isHead,
-      officeNumber,
-      contractType,
-      emailAddress,
-      rank,
-      isArchived,
-      acctType,
-      employeeID,
-    ];
+    const query = `
+      UPDATE user SET 
+        password = :password,
+        firstName = :firstName,
+        middleName = :middleName,
+        lastName = :lastName,
+        committee = :committee,
+        isHead = :isHead,
+        officeNumber = :officeNumber,
+        contractType = :contractType,
+        emailAddress = :emailAddress,
+        rank = :rank,
+        isArchived = :isArchived,
+        acctType = :acctType 
+      WHERE employeeID = :employeeID
+    `;
 
-    db.query(query, values, (err, results) => {
+    db.query(query, { ...user, employeeID }, (err, results) => {
       if (err) return reject(500);
       return resolve(results.insertId);
     });
