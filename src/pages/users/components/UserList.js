f0b8a04fc5adeb1c0296b4bbb1c04b2ed9bd92d8
Enzo1198 <lbafable@up.edu.ph>
2018-03-13 15:14:59 +0800

    Organize folder structure

--- src/pages/users/components/UserList.js
+++ src/pages/users/components/UserList.js
@@ -7,12 +7,20 @@ class UserList extends Component {
   render() {
     const { userData, expanded } = this.props;
     const gridConfig = { xxl: expanded ? 4 : 3, xl: 6, lg: 8, sm: 12, xs: 24 };
+    const sortedData = userData.sort((a, b) => {
+      if (a.lastName > b.lastName) return 1;
+      else if (a.lastName < b.lastName) return -1;
+      return 0;
+    });
 
     return (
       <Row type="flex" gutter={16}>
-        {userData.map(user => (
+        {sortedData.map(user => (
           <Col {...gridConfig}>
-            <User title={user.name} description={user.type} />
+            <User
+              title={`${user.lastName}, ${user.firstName}`}
+              description={user.type}
+            />
           </Col>
         ))}
       </Row>
