c38369f574a40a150d9e28db0ed99c95b71be527
prastudillo <prastudillo@up.edu.ph>
2018-03-13 18:48:19 +0800

    changed layout into grid

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Icon, Button, Card, Progress, Table } from 'antd';
+import { Icon, Button, Card, Progress, Table, Row, Column } from 'antd';
 import styles from './styles';
 import actions from './actions';
 import dataSource from './datasource';
@@ -9,27 +9,31 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Dashboard</h1>
-        <Card
-          title="Announcements"
-          style={styles.announcement}
-          actions={actions.map(action => <Icon type={action} />)}
-        >
-          No announcements available
-        </Card>
-        <div>
-          <Card title="Faculty Progress">
-            <Progress
-              type="dashboard"
-              percent={30}
-              style={styles.progressBar}
-            />
-            <Table
-              columns={columns}
-              dataSource={dataSource}
-              style={styles.facultyTable}
-            />
+        <Row>
+          <h1>Dashboard</h1>
+          <Card
+            title="Announcements"
+            style={styles.announcement}
+            actions={actions.map(action => <Icon type={action} />)}
+          >
+            No announcements available
           </Card>
+        </Row>
+        <div>
+          <Row>
+            <Card title="Faculty Progress">
+              <Progress
+                type="dashboard"
+                percent={30}
+                style={styles.progressBar}
+              />
+              <Table
+                columns={columns}
+                dataSource={dataSource}
+                style={styles.facultyTable}
+              />
+            </Card>
+          </Row>
         </div>
       </div>
     );
