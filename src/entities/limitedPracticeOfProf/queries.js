ec2a4e4b15cdcc11be922554ca7c6ec83681042a
Hazazen <iudacquel@up.edu.ph>
2018-03-15 23:00:42 +0800

    Add Award, Extension, and LimitedPractice

---
+++ src/entities/limitedPracticeOfProf/queries.js
@@ -0,0 +1,31 @@
+export const addLimitedPracticeOfProf = `
+  INSERT INTO limitedPracticeOfProf ( 
+    id,
+    askedPermission,
+    date 
+  )
+  VALUES ( 
+    DEFAULT,
+    :askedPermission,
+    :date
+  )
+`;
+
+export const updateLimitedPracticeOfProf = `
+  UPDATE limitedPracticeOfProf SET 
+    askedPermission=:askedPermission,
+    date=:date  
+  WHERE id=DEFAULT
+`;
+
+export const getLimitedPracticeOfProf = `
+  SELECT * FROM limitedPracticeOfProf
+  WHERE id = :id
+  ORDER BY id ASC
+  LIMIT 10;
+`;
+
+export const deleteLimitedPracticeOfProf = `
+  DELETE FROM limitedPracticeOfProf
+  WHERE id = ""
+`;
