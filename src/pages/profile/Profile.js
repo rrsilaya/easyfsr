ac4e692f2b5af1745e353926f582154cdd0f4ad3
aoponcedeleon <aoponcedeleon@up.edu.ph>
2018-03-16 20:04:15 +0800

    Revert "Layout/user profile"

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import { Row, Col, Button, Avatar } from 'antd';
-import userData from './UserContent';
-import UserCard from './UserCard';
 
 class Profile extends Component {
   render() {
     return (
       <div>
-        <UserCard userData={userData} />
+        <h1>Profile</h1>
       </div>
     );
   }
