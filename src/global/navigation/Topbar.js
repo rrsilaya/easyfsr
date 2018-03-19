a9e3a4a0660da2c052160fad149b9ef8b6bec4a3
fjmanglicmot <fjmanglicmot@up.edu.ph>
2018-03-19 16:58:53 +0800

    Add logout complete with functionality

--- src/global/navigation/Topbar.js
+++ src/global/navigation/Topbar.js
@@ -1,49 +1,11 @@
 import React, { Component } from 'react';
 import { Layout, Icon, Menu, Dropdown } from 'antd';
 import styles from './styles';
-import { connect } from 'react-redux';
-import App from '../../app/App';
-import { toggleSidebar, getSession, login } from '../../app/duck';
 
 class Topbar extends Component {
-  handleClick() {
-    const mapStateToProps = state => {
-      const {
-        isSidebarCollapsed = false,
-
-        isGettingSession = false,
-        isLoggingIn = false,
-
-        user = {},
-      } = state.app;
-
-      return {
-        isSidebarCollapsed,
-
-        isGettingSession,
-        isLoggingIn,
-
-        user,
-      };
-    };
-
-    const mapDispatchToProps = dispatch => {
-      return {
-        toggleSidebar: () => {
-          dispatch(toggleSidebar());
-        },
-        getSession: () => {
-          dispatch(getSession());
-        },
-        login: body => {
-          dispatch(login(body));
-        },
-      };
-    };
-    const Topbar = connect(mapStateToProps, mapDispatchToProps)(App);
-  }
   render() {
     const { toggleSidebar } = this.props;
+    const { logout } = this.props;
     const menu = (
       <Menu>
         <Menu.Item>
@@ -53,9 +15,9 @@ class Topbar extends Component {
           </a>
         </Menu.Item>
         <Menu.Item>
-          <a target="_blank" onclick={this.handleClick.bind(this)}>
+          <div onClick={logout}>
             <Icon type="logout" /> Logout
-          </a>
+          </div>
         </Menu.Item>
       </Menu>
     );
