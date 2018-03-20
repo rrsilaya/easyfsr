9871a9e58b134153a092692379f95aa205c14b86
Kia Mei Somabes <kssomabes@up.edu.ph>
2018-03-21 02:40:43 +0800

    Fix syntax error in DDL, add PK constraints for teachingLoad, studyLoad; add id to limitedPracticeOfProf, award and PK constraints; add utils, modify update, remove teachingLoadID from teachingLoad/queries

--- src/entities/teachingLoad/queries.js
+++ src/entities/teachingLoad/queries.js
@@ -1,12 +1,10 @@
 export const addTeachingLoad = `
 	INSERT INTO teachingLoad (
 		id,
-		teachingLoadID,
 		teachingLoadCreds
 	)
 	VALUES (
 		:id,
-		:teachingLoadID,
 		:teachingLoadCreds
 	)
 `;
@@ -14,7 +12,6 @@ export const addTeachingLoad = `
 export const updateTeachingLoad = `
 	UPDATE teachingLoad SET
 		teachingLoadCreds = :teachingLoadCreds,
-		teachingLoadID = :teachingLoadID
 	WHERE id = :id
 `;
 
@@ -27,6 +24,4 @@ export const deleteTeachingLoad = `
 export const getTeachingLoad = `
 	SELECT * FROM teachingLoad
 	WHERE id = :id
-	ORDER BY id ASC
-	LIMIT 10
 `;
