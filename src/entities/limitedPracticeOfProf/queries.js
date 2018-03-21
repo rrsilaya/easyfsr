b1aac1f2b25bcf7f138ee428aedde4b0d7d775de
Lois Quillo <llquillo@up.edu.ph>
2018-03-21 22:57:22 +0800

    Revert "Add formatQueryParams"
    This reverts commit aaba7203789acdfb59b479f9069d0013093c40f9.

--- src/entities/limitedPracticeOfProf/queries.js
+++ src/entities/limitedPracticeOfProf/queries.js
@@ -1,18 +1,3 @@
-import { formatQueryParams } from '../../utils';
-
-export const getLimitedPracticeOfProf = `
-  SELECT * FROM limitedPracticeOfProf
-  WHERE id = :id
-  ORDER BY id ASC
-  LIMIT 10;
-`;
-
-export const updateLimitedPracticeOfProf = limitedPracticeOfProf => `
-  UPDATE limitedPracticeOfProf SET 
-     ${formatQueryParams(limitedPracticeOfProf)} 
-  WHERE id=DEFAULT
-`;
-
 export const addLimitedPracticeOfProf = `
   INSERT INTO limitedPracticeOfProf ( 
     id,
@@ -24,6 +9,20 @@ export const addLimitedPracticeOfProf = `
     :askedPermission,
     :date
   )
+`;
+
+export const updateLimitedPracticeOfProf = `
+  UPDATE limitedPracticeOfProf SET 
+    askedPermission=:askedPermission,
+    date=:date  
+  WHERE id=DEFAULT
+`;
+
+export const getLimitedPracticeOfProf = `
+  SELECT * FROM limitedPracticeOfProf
+  WHERE id = :id
+  ORDER BY id ASC
+  LIMIT 10;
 `;
 
 export const deleteLimitedPracticeOfProf = `
--- src/entities/user/queries.js
+++ src/entities/user/queries.js
@@ -1,25 +1,5 @@
 import { formatQueryParams } from '../../utils';
 
-export const getUser = `
-  SELECT * from user
-  WHERE userID = :userID
-`;
-
-export const getAllUser = `
-  SELECT * FROM user;
-`;
-
-export const getUsers = (query, sortBy) => `
-  SELECT * FROM user ${query.length ? `WHERE ${formatQueryParams(query)}` : ''} 
-  ORDER BY [field] ${sortBy === 'DESC' ? 'DESC' : 'ASC'} LIMIT :limit
-`;
-
-export const updateUser = user => `
-  UPDATE user SET 
-  ${formatQueryParams(user)}
-  WHERE userID = :userID
-`;
-
 export const addUser = `
   INSERT INTO user (
     employeeID, 
@@ -43,8 +23,28 @@ export const addUser = `
   )
 `;
 
+export const updateUser = user => `
+  UPDATE user SET 
+  ${formatQueryParams(user)}
+  WHERE userID = :userID
+`;
+
+export const getAllUser = `
+  SELECT * FROM user;
+`;
+
 export const deleteUser = `
   UPDATE user SET
     isArchived = true
   WHERE userID = :userID
+`;
+
+export const getUser = `
+  SELECT * from user
+  WHERE userID = :userID
+`;
+
+export const getUsers = (query, sortBy) => `
+  SELECT * FROM user ${query.length ? `WHERE ${formatQueryParams(query)}` : ''} 
+  ORDER BY [field] ${sortBy === 'DESC' ? 'DESC' : 'ASC'} LIMIT :limit
 `;
