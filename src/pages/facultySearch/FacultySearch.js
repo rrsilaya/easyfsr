2f4b3421c23a05f277acd45fe3071666cf7b07a8
fjmanglicmot <fjmanglicmot@up.edu.ph>
2018-03-22 14:57:25 +0800

    Update faculty search

--- src/pages/facultySearch/FacultySearch.js
+++ src/pages/facultySearch/FacultySearch.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Button, Icon, Input, Table, Row, List, Col } from 'antd';
-
-import Faculty from './components/Faculty';
 import styles from './styles';
 import facultyData from './faculty-data';
 const { Item } = List;
@@ -14,11 +12,12 @@ class FacultySearch extends Component {
           placeholder="input search text"
           onSearch={value => console.log(value)}
           enterButton
-          style={{ width: 300, bottom: 20 }}
+          style={{ width: 300, top: 20 }}
         />
 
         <List
           size="large"
+          style={{ top: 50 }}
           header={
             <Item>
               <Col span={8}>
