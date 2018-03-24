71c28df8e75e56861850978fc1db25ab2a5ff88a
rrsilaya <rrsilaya@up.edu.ph>
2018-03-24 14:21:30 +0800

    Add active indicator in sidebar

--- src/global/navigation/Sidebar.js
+++ src/global/navigation/Sidebar.js
@@ -8,6 +8,23 @@ import styles from './styles';
 const { Item } = Menu;
 
 class Sidebar extends Component {
+  getActiveTab = () => {
+    const [, route] = window.location.pathname.split('/');
+
+    switch (route) {
+      case 'records':
+        return 'Service Records';
+      case 'profile':
+        return 'Profile';
+      case 'search':
+        return 'Search';
+      case 'users':
+        return 'Users';
+      case '':
+        return 'Dashboard';
+    }
+  };
+
   render() {
     const { isSidebarCollapsed } = this.props;
 
@@ -21,7 +38,11 @@ class Sidebar extends Component {
         <div style={styles.logo}>
           <img src={logo} alt="" />
         </div>
-        <Menu style={styles.sidebar} theme="dark">
+        <Menu
+          style={styles.sidebar}
+          theme="dark"
+          defaultSelectedKeys={[this.getActiveTab()]}
+        >
           {links.map(link => (
             <Item key={link.label}>
               <Link to={link.path}>
