a32329e16136073be49d1d104f87bb4ac3c69fef
Enzo1198 <lbafable@up.edu.ph>
2018-03-26 14:35:56 +0800

    Add pagination to user management page

--- src/pages/users/UsersContainer.js
+++ src/pages/users/UsersContainer.js
@@ -10,6 +10,7 @@ import {
   addUser,
   editUser,
   resetPage,
+  changeQuery,
 } from './duck';
 
 const mapStateToProps = state => {
@@ -22,6 +23,7 @@ const mapStateToProps = state => {
     isAddingUser,
     isEditingUser,
 
+    pagination,
     users,
     user,
   } = state.users;
@@ -35,6 +37,7 @@ const mapStateToProps = state => {
     isAddingUser,
     isEditingUser,
 
+    pagination,
     users,
     user,
   };
@@ -54,8 +57,8 @@ const mapDispatchToProps = dispatch => {
     changeSelectedUser: user => {
       dispatch(changeSelectedUser(user));
     },
-    getUsers: () => {
-      dispatch(getUsers());
+    getUsers: query => {
+      dispatch(getUsers(query));
     },
     addUser: values => {
       dispatch(addUser(values));
@@ -65,6 +68,9 @@ const mapDispatchToProps = dispatch => {
     },
     resetPage: () => {
       dispatch(resetPage());
+    },
+    changeQuery: query => {
+      dispatch(changeQuery(query));
     },
   };
 };
