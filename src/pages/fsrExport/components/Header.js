a76342ef5990ad32ee928e3799ff811c42473508
rrsilaya <rrsilaya@up.edu.ph>
2018-04-08 10:01:39 +0800

    Add initial styling to template

--- src/pages/fsrExport/components/Header.js
+++ src/pages/fsrExport/components/Header.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 
+const styles = {
+  header: {
+    marginBottom: '1em',
+  },
+};
+
 class Header extends Component {
   render() {
     return (
-      <div>
+      <div style={styles.header}>
         <div className="form-float">Form 67 (Revised October 25, 2013)</div>
-        <div className="center">
-          <div className="bold">FACULTY SERVICE RECORD</div>
+        <div className="center" style={{ marginBottom: '1em' }}>
+          <header className="bold">FACULTY SERVICE RECORD</header>
           <div>First Semester 2011-2012</div>
+        </div>
+        <div className="grid">
+          <div className="flex-mid" style={{ width: '75%' }}>
+            <label>PRINTED NAME:</label>
+            <div className="field-line" data-label="(Family)">
+              sdfkjjkjkjdsf
+            </div>
+            <div className="field-line" data-label="(Given)">
+              sdfkjjkjkjdsf
+            </div>
+            <div className="field-line" data-label="(MI)">
+              sdfkjjkjkjdsf
+            </div>
+          </div>
+          <div className="flex-mid margin1 left" style={{ width: '25%' }}>
+            <label>RANK:</label>
+            <div className="field-line">&nbsp;</div>
+          </div>
         </div>
       </div>
     );
