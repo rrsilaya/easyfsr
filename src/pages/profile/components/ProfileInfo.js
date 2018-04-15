b4b37ce7008b95221c7eded8d7ea2dbbf3800124
rrsilaya <rrsilaya@up.edu.ph>
2018-04-15 22:03:57 +0800

    Change profile cards to stackable grid

--- src/pages/profile/components/ProfileInfo.js
+++ src/pages/profile/components/ProfileInfo.js
@@ -1,108 +1,105 @@
 import React, { Component } from 'react';
+import { Card, Row, List } from 'antd';
 import moment from 'moment';
-import { Row, Col, Card, List } from 'antd';
+import StackGrid from 'react-stack-grid';
+
 import style from '../styles';
 
 const { Item: ListItem } = List;
 
 class ProfileInfo extends Component {
+  componentWillUpdate() {
+    if (this.grid) {
+      this.grid.updateLayout();
+    }
+  }
+
   render() {
     const { adminWork, service, isAdminWork } = this.props;
 
     const gridConfig = { xxl: 6, xl: 8, sm: 12, xs: 24 };
 
     return (
-      <Row type="flex" justify="center" gutter={16} className="gridTiles">
-        <Col {...gridConfig}>
-          <Card title="Service Records" loading>
-            Content
-          </Card>
-        </Col>
-        <Col {...gridConfig}>
-          <Card title="Awards" loading>
-            Content
-          </Card>
-        </Col>
-        <Col {...gridConfig}>
-          <Card title="Research" loading>
-            Content
-          </Card>
-        </Col>
-        <Col {...gridConfig}>
-          <Card title="Administrative Works">
-            <List
-              size="small"
-              style={style.list}
-              dataSource={adminWork}
-              renderItem={adminWork => (
-                <ListItem>
-                  <Row style={style.listItem} justify="center">
-                    <h3 className="text primary">{adminWork.position}</h3>
-                    <div>
-                      <dl>
-                        <dt>Office Unit</dt>
-                        <dd>{adminWork.officeUnit}</dd>
-                      </dl>
-                    </div>
-                  </Row>
-                </ListItem>
-              )}
-            />
-          </Card>
-        </Col>
-        <Col {...gridConfig}>
-          <Card title="Community Service">
-            <List
-              size="small"
-              style={style.list}
-              dataSource={service}
-              renderItem={service => (
-                <ListItem>
-                  <Row style={style.listItem} justify="center">
-                    <h3 className="text primary">{service.title}</h3>
-                    <div>
-                      <dl>
-                        <dt>Type</dt>
-                        <dd>{service.type}</dd>
-                      </dl>
-                      <dl>
-                        <dt>Role</dt>
-                        <dd>{service.role}</dd>
-                      </dl>
-                      <dl>
-                        <dt>Participants</dt>
-                        <dd>{service.participant}</dd>
-                      </dl>
-                      <dl>
-                        <dt>Date</dt>
-                        <dd>
-                          {moment(service.startDate).format('MMMM D, YYYY')} to{' '}
-                          {moment(service.endDate).format('MMMM D, YYYY')}
-                        </dd>
-                      </dl>
-                    </div>
-                  </Row>
-                </ListItem>
-              )}
-            />
-          </Card>
-        </Col>
-        <Col {...gridConfig}>
-          <Card title="Creative Works" loading>
-            Content
-          </Card>
-        </Col>
-        <Col {...gridConfig}>
-          <Card title="Limited Practices" loading>
-            Content
-          </Card>
-        </Col>
-        <Col {...gridConfig}>
-          <Card title="Study Load" loading>
-            Content
-          </Card>
-        </Col>
-      </Row>
+      <StackGrid
+        columnWidth="33.3333%"
+        gutterWidth={16}
+        gutterHeight={16}
+        gridRef={grid => (this.grid = grid)}
+      >
+        <Card title="Service Records" loading>
+          Content
+        </Card>
+        <Card title="Awards" loading>
+          Content
+        </Card>
+        <Card title="Research" loading>
+          Content
+        </Card>
+        <Card title="Administrative Works">
+          <List
+            size="small"
+            style={style.list}
+            dataSource={adminWork}
+            renderItem={adminWork => (
+              <ListItem>
+                <Row style={style.listItem} justify="center">
+                  <h3 className="text primary">{adminWork.position}</h3>
+                  <div>
+                    <dl>
+                      <dt>Office Unit</dt>
+                      <dd>{adminWork.officeUnit}</dd>
+                    </dl>
+                  </div>
+                </Row>
+              </ListItem>
+            )}
+          />
+        </Card>
+        <Card title="Community Service">
+          <List
+            size="small"
+            style={style.list}
+            dataSource={service}
+            renderItem={service => (
+              <ListItem>
+                <Row style={style.listItem} justify="center">
+                  <h3 className="text primary">{service.title}</h3>
+                  <div>
+                    <dl>
+                      <dt>Type</dt>
+                      <dd>{service.type}</dd>
+                    </dl>
+                    <dl>
+                      <dt>Role</dt>
+                      <dd>{service.role}</dd>
+                    </dl>
+                    <dl>
+                      <dt>Participants</dt>
+                      <dd>{service.participant}</dd>
+                    </dl>
+                    <dl>
+                      <dt>Date</dt>
+                      <dd>
+                        {moment(service.startDate).format('MMMM D, YYYY')} to{' '}
+                        {moment(service.endDate).format('MMMM D, YYYY')}
+                      </dd>
+                    </dl>
+                  </div>
+                </Row>
+              </ListItem>
+            )}
+          />
+        </Card>
+        <Card title="Creative Works" loading>
+          Content
+        </Card>
+        <Card title="Limited Practices" loading>
+          Content
+        </Card>
+        <Card title="Study Load" loading>
+          Content
+        </Card>
+      </StackGrid>
     );
   }
 }
