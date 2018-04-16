448beee8a9d3cecc4c52617c2c03dae01a55dc21
Erica Mae Yeban <camaeyeban@gmail.com>
2018-04-16 17:29:12 +0800

    Hide dashboard to ordinary user

--- src/app/routes.js
+++ src/app/routes.js
@@ -8,36 +8,50 @@ import FsrExport from '../pages/fsrExport/FsrExportContainer';
 
 const routes = [
   {
+    type: 'route',
     path: '/',
     exact: true,
+    restricted: true,
     component: Dashboard,
   },
   {
+    type: 'redirect',
+    from: '/',
+    exact: true,
+    to: '/records',
+  },
+  {
+    type: 'route',
     path: '/profile/:userID',
     exact: true,
     component: Profile,
   },
   {
+    type: 'route',
     path: '/records',
     exact: true,
     component: ServiceRecords,
   },
   {
+    type: 'route',
     path: '/records/:fsrID',
     exact: true,
     component: FSRForm,
   },
   {
+    type: 'route',
     path: '/records/:fsrID/preview',
     exact: true,
     component: FsrExport,
   },
   {
+    type: 'route',
     path: '/users',
     component: Users,
     restricted: true,
   },
   {
+    type: 'route',
     path: '/search',
     component: FacultySearch,
   },
