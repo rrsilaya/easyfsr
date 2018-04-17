6636810eb454ed1443dc052cef2d2eb82786199e
Kia Mei Somabes <kssomabes@up.edu.ph>
2018-04-18 06:47:06 +0800

    Add computation for course credit, student credit units, teaching load credits with and without multiplier

--- src/entities/subject/controller.js
+++ src/entities/subject/controller.js
@@ -63,6 +63,51 @@ export const getSubject = ({ subjectID }) => {
   });
 };
 
+export const computeSubject = subject => {
+  return new Promise((resolve, reject) => {
+    const {
+      subjectCode,
+      teachingLoadCreds,
+      noOfStudents,
+      sectionCode,
+      hoursPerWeek,
+    } = subject;
+
+    const grad = new RegExp('[234][0-9]{2}D*');
+    const lab = new RegExp('.+L');
+    const sp1 = new RegExp('CMSCs*190s*-s*1');
+    const sp2 = new RegExp('CMSCs*190s*-s*2');
+    const it = new RegExp('ITs*1D*');
+
+    const courseCred = lab.test(sectionCode) ? 1 : hoursPerWeek;
+
+    const studCredUnits = noOfStudents * courseCred;
+
+    const TLC = lab.test(sectionCode)
+      ? 1.5
+      : sp1.test(subjectCode) ? 1 : sp2.test(subjectCode) ? 2 : hoursPerWeek;
+
+    let TLCM = 0;
+    if (!grad.test(subjectCode)) {
+      TLCM =
+        noOfStudents <= 40
+          ? TLC
+          : noOfStudents > 160
+            ? TLC * 2
+            : TLC * ((noOfStudents - 40) / 120 + 1);
+    } else if (sp1.test(subjectCode) || sp2.test(subjectCode))
+      TLCM = noOfStudents / 2 * TLC * 3;
+
+    it.test(subjectCode) ? (TLCM = TLCM * 1.33) : TLCM;
+
+    subject.courseCred = courseCred;
+    subject.studCredUnits = studCredUnits;
+    subject.TLC = TLC;
+    subject.TLCM = TLCM;
+    return resolve(subject);
+  });
+};
+
 export const getSubjects = subject => {
   return new Promise((resolve, reject) => {
     db.query(
