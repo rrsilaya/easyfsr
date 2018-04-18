ae21365250e91a6719b2cbb5d18017464305626c
Kia Mei Somabes <kssomabes@up.edu.ph>
2018-04-18 23:33:37 +0800

    Update seed data of user and fsr

--- src/seed/data2.sql
+++ src/seed/data2.sql
@@ -15,7 +15,20 @@ INSERT INTO user (employeeID, password, firstName, middleName, lastName, committ
 INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000359994', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Rick Jason', 'G', 'Obrero', 'Research', 'C-118', 'FULL-TIME', 'rgobrero@up.edu.ph', 'Instructor 1');
 INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000385706', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Kristine Bernadette', 'P', 'Pelaez', 'Registration', 'C-112', 'FULL-TIME', 'kppelaez@up.edu.ph', 'Instructor 3');
 INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000737258', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Miyah', 'D', 'Queliste', 'Extension', 'C-115', 'PART-TIME', 'mdqueliste@up.edu.ph', 'Instructor 3');
-
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000375233', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Lailanie', 'Danila', 'R', 'Curriculum & Instruction', 'C-116', 'FULL-TIME', 'lrdanila@up.edu.ph', 'Assistant Professor 1');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000773864', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Joseph Anthony', 'C', 'Hermocilla', 'Sports', 'C-112', 'FULL-TIME', 'jchermocilla@up.edu.ph', 'Assistant Professor 5');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000730899', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Arian', 'J', 'Jacildo', 'Registration', 'C-116', 'FULL-TIME', 'ajjacildo@up.edu.ph', 'Assistant Professor 5');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000089260', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Concepcion', 'L', 'Khan', 'Extension', 'C-112', 'FULL-TIME', 'clkhan@up.edu.ph', 'Assistant Professor 7');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000477064', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Fermin Roberto', 'G', 'Lapitan', 'Curriculum & Instruction', 'C-112', 'FULL-TIME', 'fglapitan@up.edu.ph', 'Assistant Professor 2');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000380417', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Val Randolf', 'M', 'Madrid', 'Extension', 'C-118', 'FULL-TIME', '@up.edu.ph ', 'Assistant Professor 3');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000741254', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Katrina Joy', 'H', 'Magno', 'Sports', 'C-114', 'FULL-TIME', 'khmagno@up.edu.ph', 'Assistant Professor 2');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000795879', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Rozano', 'S', 'Maniaol', 'Extension', 'C-118', 'FULL-TIME', 'rsmaniaol@up.edu.ph', 'Assistant Professor 1');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000260529', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Danilo', 'J', 'Mercado', 'Extension', 'C-113', 'FULL-TIME', 'djmercado@up.edu.ph', 'Assistant Professor 2');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000319408', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Rizza', 'DC', 'Mercado', 'Registration', 'C-116', 'FULL-TIME', 'rdmercado2@up.edu.ph', 'Assistant Professor 4');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000167276', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Toni-Jan Keith', 'P', 'Monserrat', 'Alumni Relations', 'C-113', 'FULL-TIME', 'tjkpmonserrat@up.edu.ph', 'Assistant Professor 1');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000342420', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Jaderick', 'P', 'Pabico', 'Sports', 'C-118', 'FULL-TIME', 'jppabico@up.edu.ph ', 'Professor 2');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank) VALUES ('1000277695', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Reginald Neil', 'C', 'Recario', 'Practicum', 'C-110', 'FULL-TIME', 'rcrecario@up.edu.ph', 'Assistant Professor 1');
+INSERT INTO user (employeeID, password, firstName, middleName, lastName, committee, officeNumber, contractType, emailAddress, rank, acctType) VALUES ('1000597752', '$2a$10$JQL/6dENt1TQofx49huAmu1e/K/m8UPn4SGXixRU5NYDK/QzpudbW', 'Jaime', 'M', 'Samaniego', 'Curriculum & Instruction', 'C-113', 'FULL-TIME', 'jmsamaniego2@up.edu.ph', 'ICS Director', 'ADMIN');
 
 INSERT INTO meta (acadYear, semester, universityRegistrar, homeDepartment, formRevision, homeCollege) VALUES ('2017-2018', 'Second', 'Myrna G. Carandang', 'Institute of Computer Science', '2013-10-25', 'CAS');
 
@@ -50,3 +63,33 @@ INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (14, '2017-2018', 'F
 INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (14, '2016-2017', 'Second', 1);
 INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (15, '2017-2018', 'Second', 1);
 INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (15, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (16, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (16, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (17, '2016-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (17, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (18, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (18, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (19, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (19, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (20, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (20, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (21, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (21, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (22, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (22, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (23, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (23, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (24, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (24, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (25, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (25, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (26, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (26, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (27, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (27, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (28, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (28, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (29, '2017-2018', 'First', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (29, '2016-2017', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (30, '2017-2018', 'Second', 1);
+INSERT INTO fsr (userID, acadYear, semester, metaID) VALUES (30, '2017-2018', 'First', 1);
