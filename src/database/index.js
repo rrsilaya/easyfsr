002b565e98439f876bf241e6ff3bf3695933e3a7
rrsilaya <rrsilaya@up.edu.ph>
2018-04-25 08:44:18 +0800

    Change database connection to pool

--- src/database/index.js
+++ src/database/index.js
@@ -1,68 +1,64 @@
 import mysql from 'mysql';
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: 'localhost',
   user: 'easyfsr',
   password: 'admin',
-  db: 'easyfsr',
+  database: 'easyfsr',
 });
 
-db.on('ready', () => console.log('Database is connected')).on('error', err => {
-  console.log('Error in connecting to database');
-  console.log(err.message);
-});
-
-db.connect(err => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.log('Error in connecting to database');
     console.log(err.message);
   } else {
     console.log('Database is connected');
   }
 });
 
-db.config.queryFormat = function(query, values) {
-  if (!values) return query;
+db.on('connection', connection => {
+  connection.config.queryFormat = function(query, values) {
+    if (!values) return query;
 
-  if (Array.isArray(values)) {
-    let offset = 0;
+    if (Array.isArray(values)) {
+      let offset = 0;
 
-    return query.replace(
-      /(\?{1,2})/g,
-      function(text, match) {
-        offset++;
+      return query.replace(
+        /(\?{1,2})/g,
+        function(text, match) {
+          offset++;
 
-        if (offset > values.length) return text;
+          if (offset > values.length) return text;
 
-        if (match.length === 2) return this.escapeId(values[offset - 1]);
-        else return this.escape(values[offset - 1]);
-      }.bind(this),
-    );
-  } else {
-    const escaped = query.replace(
-      /\:(\w+)/g,
-      function(text, key) {
-        if (values.hasOwnProperty(key)) {
-          return this.escape(values[key]);
-        }
+          if (match.length === 2) return this.escapeId(values[offset - 1]);
+          else return this.escape(values[offset - 1]);
+        }.bind(this),
+      );
+    } else {
+      const escaped = query.replace(
+        /\:(\w+)/g,
+        function(text, key) {
+          if (values.hasOwnProperty(key)) {
+            return this.escape(values[key]);
+          }
 
-        return text;
-      }.bind(this),
-    );
+          return text;
+        }.bind(this),
+      );
 
-    return escaped.replace(
-      /\[(\w+)\]/g,
-      function(text, key) {
-        if (values.hasOwnProperty(key)) {
-          return this.escapeId(values[key]);
-        }
+      return escaped.replace(
+        /\[(\w+)\]/g,
+        function(text, key) {
+          if (values.hasOwnProperty(key)) {
+            return this.escapeId(values[key]);
+          }
 
-        return text;
-      }.bind(this),
-    );
-  }
-};
+          return text;
+        }.bind(this),
+      );
+    }
+  };
 
-db.query('USE easyfsr');
+  connection.query('USE easyfsr');
+});
 
 export default db;
